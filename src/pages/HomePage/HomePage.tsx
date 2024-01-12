d351e4fe111204afdd834d3ccd97aeb9f7dca7f5
unknown <antoine_grubert@hotmail.fr>
2024-01-12 15:31:07 +0100

    feat : create homepage and start styling

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,24 @@
 import './HomePage.scss'
+import Header from '../../components/Header/Header'
 
 const HomePage = () => {
     return (
-        <>
-            <h1> Home Page </h1>
-        </>
+        <div className='home-page'>
+            <Header />
+            <main className='home-main'>
+                <section className='home-firstpage'>
+                    <button type='button' className='button black'> CONNEXION </button>
+                    <button type='button' className='button white'> INSCRIPTION </button>
+                    <div className="show-more">
+                        <p> Voir plus </p>
+                        <i className="icon fa-solid fa-chevron-down"></i>
+                    </div>
+                </section>
+                <section className='home-secondpage'>
+                    <p> Texte </p>
+                </section>
+            </main>
+        </div>
     )
 }
 

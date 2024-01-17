69e8b6c31e9feb015c8e2566671aa01837d39c8f
unknown <antoine_grubert@hotmail.fr>
2024-01-17 10:12:42 +0100

    feat : add logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,23 @@
-import './Header.scss'
+import './Header.scss';
+import { useAuth } from '../../context/AuthContext';
 
 const Header = () => {
+
+    const { logout, isAuthenticated } = useAuth()!; //Hook to get logout function from AuthContext, exclamation mark to tell TS that it's not null
+
+    const handleLogout = () => {
+        logout();
+    }
 
     return (
         <>
             <header className="header">
-                <div className="header__logo">
-                    <img src="/assets/logo/maxrep-logo.png" alt="" />
-                </div>
                 <div className="header__title">
+                    <img src="/assets/logo/maxrep-logo.png" alt="" />
                     <h1> MaxRep </h1>
+                </div>
+                <div className="header__logout">
+                    {isAuthenticated() && <i className="fa-solid fa-right-from-bracket" title='Se déconnecter' onClick={handleLogout}></i>}
                 </div>
             </header>
         </>

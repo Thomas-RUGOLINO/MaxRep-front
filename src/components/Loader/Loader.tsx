588d18216847eb07183e00acd662288763735657
unknown <antoine_grubert@hotmail.fr>
2024-01-18 14:05:52 +0100

    feat : create loader component &  add it in profile page

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import './Loader.scss'
+
+const Loader = () => {
+    return (
+        <>
+            <div className="loader-container">
+                <span className="loader"></span>
+            </div>
+        </>
+    )
+}
+
+export default Loader
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -11,6 +11,7 @@ import EditProfileForm from '../../components/Forms/EditProfileForm';
 import AddSportForm from '../../components/Forms/AddSportForm';
 import DeleteSportForm from '../../components/Forms/DeleteSportForm';
 import ErrorPage from '../ErrorPage/ErrorPage';
+import Loader from '../../components/Loader/Loader';
 import calculateAgeFromBirthDate from '../../utils/calculateAgeFromBirthDate';
 import formatUserName from '../../utils/formatUserName';
 
@@ -58,7 +59,7 @@ interface UserSessionProps {
 const ProfilePage = () => {
 
     const [userInfos, setUserInfos] = useState<UserInfosProps | null>(null);
-    //! Add a loader state
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<ErrorProps | null>(null);
     //Modal states
     const [isEditProfileModalOpen, setIsEditProfileModalOpen] = useState<boolean>(false);
@@ -80,7 +81,7 @@ const ProfilePage = () => {
     },[isAuthenticated, navigate])
 
     //Get user profile infos
-    const getUserProfile = async () => {  //! ==> Déplacer cette fonction dans le dossier services
+    const getUserProfile = async () => { 
 
         const token = localStorage.getItem('userToken');
 
@@ -90,18 +91,24 @@ const ProfilePage = () => {
             const userId = decodedTokenProps.id;
 
             try {
+                setIsLoading(true);
+                setError(null);
+
                 const response = await axios.get(`https://maxrep-back.onrender.com/api/profile/${userId}` , {
                     headers: {
                         'Authorization': `Bearer ${token}` //Send token to backend to verify user
                     }
                 });
-                //! Add a loader 
+                
                 console.log(response.data);
                 
                 //== Case response is ok
                 if (response.status === 200) {
                     setUserInfos(response.data);
-                } 
+
+                } else {
+                    setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+                }
 
             } catch (error) {
                 if (axios.isAxiosError(error)) { //== Case if axios error
@@ -114,8 +121,16 @@ const ProfilePage = () => {
     
                 } else { //== Case if not axios error
                     setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
-                }
-                console.log(error);
+                    console.log(error);
+                }                 
+
+            } finally {
+                //add a interval to show loader
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 1000);
+
+                // setIsLoading(false);
             }
 
         //If there is no token
@@ -123,7 +138,6 @@ const ProfilePage = () => {
             setError({status:401, message:'Unauthorized / Non autorisé'});
         }
 
-        
     }
 
     const handleProfileUpdate = () => {
@@ -156,123 +170,127 @@ const ProfilePage = () => {
     };
 
     //Handle 3 cases => error, loading and userInfos received
-    return (
-        <> 
-        {error && <ErrorPage status={error.status} message={error.message} />}
-        {!error && userInfos && (
-            <>
-                <Header />
-                <NavMenu />
-                <div className='profile-page'>
-                    <header className="profile-header">
-                        <h2> Profil </h2>
-                    </header>
-                    <main className="profile-main">
-                        <section className="profile__head">
-                            <div className="picture">
-                                <img src={userInfos.profile_picture} alt="profile-picture"/>
-                            </div>
-                            <div className="name">
-                                <h3> {formatUserName(userInfos.firstname)} </h3>
-                                <h3> {formatUserName(userInfos.lastname)} </h3>
-                            </div>
-                        </section>
-                        <section className="profile__infos">
-                            <div className="container">
-                                <div className="container__header">
-                                    <h3> Infos </h3>
-                                    <i onClick={openEditProfileModal} className="icon fa-solid fa-pen-to-square" title='Editer les infos'></i>
-                                </div>
-                                <div className="container__content">
-                                    <div className="info age">
-                                        <p> <strong>Age</strong> </p>
-                                        <p> {calculateAgeFromBirthDate(userInfos.birth_date)} ans </p>
-                                    </div>
-                                    <div className="info gender">
-                                        <p> <strong>Sexe</strong> </p>
-                                        <p> {userInfos.gender} </p>
-                                    </div>
-                                    <div className="info city">
-                                        <p> <strong>Ville</strong> </p>
-                                        <p> {userInfos.city ? userInfos.city : "Non renseignée"} </p>
-                                    </div>
-                                    <div className="info country">
-                                        <p> <strong>Pays</strong> </p>
-                                        <p> {userInfos.country ? userInfos.country : "Non renseigné"} </p>
-                                    </div>
-                                    <div className="info height">
-                                        <p> <strong>Taille</strong> </p>
-                                        <p> {userInfos.height ? userInfos.height + 'cm' : "Non renseigné"} </p>
-                                    </div>
-                                    <div className="info weight">
-                                        <p> <strong>Poids</strong> </p>
-                                        <p> {userInfos.weight ? userInfos.weight + 'kg' : "Non renseigné"} </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        <section className="profile__sports">
-                            <div className="container">
-                                <div className="container__header">
-                                    <h3> Sports </h3>
-                                    <i onClick={openAddSportModal} className="icon fa-regular fa-square-plus" title='Ajouter un sport'></i>
-                                </div>
-                                <table className="sports-table" cellSpacing="10">
-                                    <thead>
-                                        <tr> 
-                                            <th>  </th>
-                                            <th> Sports suivis </th>
-                                            <th> Dernière perf </th>   
-                                            <th>  </th>   
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {userInfos.sports.length > 0 && userInfos.sports.map(sport => (
-                                            <tr key={sport.id}>
-                                                <td> <i className="icon-table fa-solid fa-chart-line"></i> </td>
-                                                <td> {sport.name} </td>
-                                                <td>
-                                                     {getMostRecentSessionScore(userInfos.id, sport.id) ? 
-                                                    (`${getMostRecentSessionScore(userInfos.id, sport.id)} ${sport.unit}`
-                                                    ) : ("Aucune donnée")}
-                                                </td>
-                                                <td> <i onClick={() => openDeleteSportModal(sport.id)} className="icon-table fa-solid fa-circle-xmark" title='Supprimer un sport'></i> </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </section>
-                    </main>
-                </div>
+    if (error) {
+        return <ErrorPage status={error.status} message={error.message} />
+    }
 
-                {/* Modals are displayed on user's clicks */}
-                <Modal title='Editer mes infos' isOpen={isEditProfileModalOpen} onClose={closeEditProfileModal}> 
-                    <EditProfileForm 
-                        userId={userInfos.id}
-                        userCurrentInfos={userInfos}
-                        onClose={closeEditProfileModal}
-                        onProfileUpdate={handleProfileUpdate}
-                    />
-                </Modal>
-                <Modal title='Ajouter un sport' isOpen={isAddSportModalOpen} onClose={closeAddSportModal}> 
-                    <AddSportForm
-                        userId={userInfos.id}
-                        onClose={closeAddSportModal}
-                        onProfileUpdate={handleProfileUpdate}
-                    />
-                </Modal>
-                <Modal title='Supprimer le sport' isOpen={isDeleteSportModalOpen} onClose={closeDeleteSportModal}> 
-                    <DeleteSportForm
-                        userId={userInfos.id}
-                        sportId={selectedSportId}
-                        onClose={closeDeleteSportModal}
-                        onProfileUpdate={handleProfileUpdate}
-                    />
-                </Modal>
-            </>
-            )}
-        
+    return (
+        <>
+            <Header />
+            <NavMenu />
+            <div className="profile-page">
+                {isLoading ? ( 
+                    <Loader /> 
+                ) : (
+                    userInfos &&  (
+                        <>
+                            <header className="profile-header">
+                                <h2> Profil </h2>
+                            </header>
+                            <main className="profile-main">
+                                <section className="profile__head">
+                                    <div className="picture">
+                                        <img src={userInfos.profile_picture} alt="profile-picture"/>
+                                    </div>
+                                    <div className="name">
+                                        <h3> {formatUserName(userInfos.firstname)} </h3>
+                                        <h3> {formatUserName(userInfos.lastname)} </h3>
+                                    </div>
+                                </section>
+                                <section className="profile__infos">
+                                    <div className="container">
+                                        <div className="container__header">
+                                            <h3> Infos </h3>
+                                            <i onClick={openEditProfileModal} className="icon fa-solid fa-pen-to-square" title='Editer les infos'></i>
+                                        </div>
+                                        <div className="container__content">
+                                            <div className="info age">
+                                                <p> <strong>Age</strong> </p>
+                                                <p> {calculateAgeFromBirthDate(userInfos.birth_date)} ans </p>
+                                            </div>
+                                            <div className="info gender">
+                                                <p> <strong>Sexe</strong> </p>
+                                                <p> {userInfos.gender} </p>
+                                            </div>
+                                            <div className="info city">
+                                                <p> <strong>Ville</strong> </p>
+                                                <p> {userInfos.city ? userInfos.city : "Non renseignée"} </p>
+                                            </div>
+                                            <div className="info country">
+                                                <p> <strong>Pays</strong> </p>
+                                                <p> {userInfos.country ? userInfos.country : "Non renseigné"} </p>
+                                            </div>
+                                            <div className="info height">
+                                                <p> <strong>Taille</strong> </p>
+                                                <p> {userInfos.height ? userInfos.height + 'cm' : "Non renseigné"} </p>
+                                            </div>
+                                            <div className="info weight">
+                                                <p> <strong>Poids</strong> </p>
+                                                <p> {userInfos.weight ? userInfos.weight + 'kg' : "Non renseigné"} </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                                <section className="profile__sports">
+                                    <div className="container">
+                                        <div className="container__header">
+                                            <h3> Sports </h3>
+                                            <i onClick={openAddSportModal} className="icon fa-regular fa-square-plus" title='Ajouter un sport'></i>
+                                        </div>
+                                        <table className="sports-table" cellSpacing="10">
+                                            <thead>
+                                                <tr> 
+                                                    <th>  </th>
+                                                    <th> Sports suivis </th>
+                                                    <th> Dernière perf </th>   
+                                                    <th>  </th>   
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {userInfos.sports.length > 0 && userInfos.sports.map(sport => (
+                                                    <tr key={sport.id}>
+                                                        <td> <i className="icon-table fa-solid fa-chart-line"></i> </td>
+                                                        <td> {sport.name} </td>
+                                                        <td>
+                                                                {getMostRecentSessionScore(userInfos.id, sport.id) ? 
+                                                            (`${getMostRecentSessionScore(userInfos.id, sport.id)} ${sport.unit}`
+                                                            ) : ("Aucune donnée")}
+                                                        </td>
+                                                        <td> <i onClick={() => openDeleteSportModal(sport.id)} className="icon-table fa-solid fa-circle-xmark" title='Supprimer un sport'></i> </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </section>
+                            </main>
+                            <Modal title='Editer mes infos' isOpen={isEditProfileModalOpen} onClose={closeEditProfileModal}> 
+                                <EditProfileForm 
+                                    userId={userInfos.id}
+                                    userCurrentInfos={userInfos}
+                                    onClose={closeEditProfileModal}
+                                    onProfileUpdate={handleProfileUpdate}
+                                />
+                                </Modal>
+                            <Modal title='Ajouter un sport' isOpen={isAddSportModalOpen} onClose={closeAddSportModal}> 
+                                <AddSportForm
+                                    userId={userInfos.id}
+                                    onClose={closeAddSportModal}
+                                    onProfileUpdate={handleProfileUpdate}
+                                />
+                            </Modal>
+                            <Modal title='Supprimer le sport' isOpen={isDeleteSportModalOpen} onClose={closeDeleteSportModal}> 
+                                <DeleteSportForm
+                                    userId={userInfos.id}
+                                    sportId={selectedSportId}
+                                    onClose={closeDeleteSportModal}
+                                    onProfileUpdate={handleProfileUpdate}
+                                />
+                            </Modal>                                            
+                        </>
+                    )
+                ) }
+            </div>
         </>
     )
 }

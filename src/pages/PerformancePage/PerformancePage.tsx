4b53b8256a55d27b2ffefafa7c1c90be17932792
unknown <antoine_grubert@hotmail.fr>
2024-01-22 15:29:10 +0100

    feat : get sessions from user in perf page & map on it

--- src/pages/PerformancePage/PerformancePage.tsx
+++ src/pages/PerformancePage/PerformancePage.tsx
@@ -1,40 +1,137 @@
 import './PerformancePage.scss'
 import Header from '../../components/Header/Header';
 import NavMenu from '../../components/NavMenu/NavMenu';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import axios from 'axios';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import Loader from '../../components/Loader/Loader';
+
+interface ErrorProps {
+    status:number,
+    message:string
+}
+
+interface SportProps {
+    id:number,
+    name:string,
+    unit:string,
+    sessions:SessionProps[]
+}
+
+interface SessionProps { 
+    id:number,
+    date:string,
+    description:string,
+    score:number,
+    sport_id:number,
+}
 
 const PerformancePage = () => {
+
+    const navigate = useNavigate(); //Hook to navigate to another page
+    const { isAuthenticated, token, userId } = useAuth()!; //Hook to get token and userId from AuthContext if user is authenticated
+
+    const [userPerformances, setUserPerformances] = useState<SportProps[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<ErrorProps | null>(null);
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate('/login');
+
+        } else {
+            getUserPerformances();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[isAuthenticated, navigate, token, userId]);
+
+    const getUserPerformances = async () => {
+
+        if (!userId) {
+            setError({status:401, message:'Unauthorized / Non autorisé'});
+        }
+
+        try {
+            setIsLoading(true);
+            setError(null);
+
+            const response = await axios.get(`https://maxrep-back.onrender.com/api/performances/${userId}` , {
+                headers: {
+                    'Authorization': `Bearer ${token}` //Send token to backend to verify user
+                }
+            });
+            
+            console.log(response.data);
+            
+            //== Case response is ok
+            if (response.status === 200) {
+                setUserPerformances(response.data.sports);
+
+            } else {
+                setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+            }
+
+        } catch (error) {
+            setIsLoading(false);
+            
+            if (axios.isAxiosError(error)) { //== Case if axios error
+                if (error.response) {
+                    setError({status:error.response.status, message:error.response.data.error});
+
+                } else { //== Case if no response from server
+                    setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+                }
+
+            } else { //== Case if not axios error
+                setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+                console.error(error);
+            }                 
+
+        } finally {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 500);
+        }
+    }
+
+    //Handle 3 cases => error, loading and userInfos received
+    if (error) {
+        return <ErrorPage status={error.status} message={error.message} />
+    }
+
     return (
         <>
             <Header />
             <NavMenu />
             <div className="performance-page">
-                <header className="performance-header">
-                    <h2> Performance </h2>
-                </header>
-                <main>
-                    <div className="sports-list">
-                        <article className="sport">
-                            <div className="sport__header">
-                                <h3> Titre du sport </h3>
-                                <i className="fa-solid fa-chevron-down"></i>
+                {isLoading && <Loader /> ? (
+                    <Loader isPage />
+                ) : (
+                    <>
+                        <header className="performance-header">
+                            <h2> Performance </h2>
+                        </header>
+                        <main>
+                            <div className="sports-list">
+                                {userPerformances && userPerformances.map((sport: SportProps) => (
+                                    <article key={sport.id} className="sport">
+                                        <div className="sport__header">
+                                            <h3> {sport.name} </h3>
+                                            <i className="fa-solid fa-chevron-down"></i>
+                                        </div>
+                                        <div className="sport__content">
+                                            <p> React Charts </p>
+                                            <p> Ajouter une perf...</p>
+                                        </div>
+                                    </article>
+                                ))}
                             </div>
-                            <div className="sport__content">
-                                <p> React Charts </p>
-                                <p> Ajouter une perf...</p>
-                            </div>
-                        </article>
-                        <article className="sport">
-                            <div className="sport__header">
-                                <h3> Titre du sport </h3>
-                                <i className="fa-solid fa-chevron-down"></i>
-                            </div>
-                            <div className="sport__content">
-                                <p> React Charts </p>
-                                <p> Ajouter une perf ...</p>
-                            </div>
-                        </article>
-                    </div>
-                </main>
+                        </main>
+                    </>
+                )}
+                
             </div>
         </>
     )

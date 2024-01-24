b48bd7c1643f231fc8e69456d9c07a097c551e1c
Charles Robart <robartcharles@gmail.com>
2024-01-24 16:28:11 +0100

    feat: dynamic data loading from userInfos

--- src/pages/RankingPage/RankingPage.tsx
+++ src/pages/RankingPage/RankingPage.tsx
@@ -2,10 +2,10 @@ import './RankingPage.scss'
 import Header from '../../components/Header/Header';
 import NavMenu from '../../components/NavMenu/NavMenu';
 import { useAuth } from '../../context/AuthContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-//import { countryNames } from '../../data/countriesList';
+import { countryNames } from '../../data/countriesList';
 
 interface ErrorProps {
     status:number,
@@ -27,16 +27,6 @@ interface UserProps {
     country: string | null;
     firstname: string;
     lastname: string;
-    // autres champs...
-}
-
-interface UserInfosProps {
-    country: string,
-    gender: string,
-    firstname: string,
-    lastname: string,
-    weight: number
-
 }
 
 interface QueryParamsProps {
@@ -51,11 +41,10 @@ const RankingPage = () => {
 
     const [error, setError] = useState<ErrorProps | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    // const [ranking, setRanking] = useState<RankingProps[]>([]);
-    // const [userInfos, setUserInfos] = useState<UserInfosProps>({});
+    const [ranking, setRanking] = useState<RankingProps[]>([]);
     const [queryParams, setQueryParams] = useState<QueryParamsProps>({
-        sportId: 3,
-        gender: 'Homme',
+        sportId: 4,
+        gender: '',
         country: '',
         weightMin: '',
         weightMax: ''
@@ -65,22 +54,52 @@ const RankingPage = () => {
         console.log('queryParms : ' ,queryParams)
     }, [queryParams])
 
-
     const navigate = useNavigate();
     const { isAuthenticated, token, userId } = useAuth()!;
 
-    useEffect(() => {
-        if (!isAuthenticated()) {
-            navigate('/login');
+    const getBestScores =  useCallback (async (queryParams: QueryParamsProps) => {
 
-        } else {
-            // getUserInfos();
+        try{
+            setIsLoading(true);
+            setError(null);
+
+            // Récupérer les données de la table ranking en fonction du sport de l'utilisateur
+            const response = await axios.get(`https://maxrep-back.onrender.com/api/ranking?sportId=${queryParams.sportId}&gender=${queryParams.gender}&country=${queryParams.country}&weightMin=${queryParams.weightMin}&weightMax=${queryParams.weightMax}` , {
+                headers: {
+                    'Authorization': `Bearer ${token}` //Send token to backend to verify user
+                }
+            });
+
+            console.log('bestScores :' , response.data);
+            // sort by best_score and organize rank id by order
+            const rank = response.data.sort((a: RankingProps, b: RankingProps) => (a.best_score > b.best_score) ? -1 : 1)
+            
+            setRanking(rank);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[isAuthenticated, navigate, token, userId]);
 
-    const getBestScores = async (queryParams: QueryParamsProps) => {
-        
+        catch(error) {
+            setIsLoading(false);
+            
+            if (axios.isAxiosError(error)) { //== Case if axios error
+                if (error.response) {
+                    setError({status:error.response.status, message:error.response.data.error});
+
+                } else { //== Case if no response from server
+                    setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+                }
+
+            } else { //== Case if not axios error
+                setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
+                console.error(error);
+            }                 
+
+        } finally {
+            setIsLoading(false);
+        }
+    }, [token]);
+
+    const getUserInfos = async () => {
+
         if (!userId) {
             setError({status:401, message:'Unauthorized / Non autorisé'});
         }
@@ -89,22 +108,24 @@ const RankingPage = () => {
             setIsLoading(true);
             setError(null);
 
-            // console.log("userSportID : " , userSportID);
-            // console.log("userGender: ", userGender);
-
-            //URL Call API complète : 
-           // http://maxrep-back.onrender.com/api/ranking?sportId=3&gender&country=Argentine&weightMin=50&weightMax=60
-
-
-            // récupérer les données de la table ranking en fonction du sport de l'utilisateur
-            const response = await axios.get(`https://maxrep-back.onrender.com/api/ranking?sportId=${queryParams.sportId}&gender=${queryParams.gender}&country=${queryParams.country}&weightMin=${queryParams.weightMin}&weightMax=${queryParams.weightMax}` , {
+            // Récupérer les données de la table ranking en fonction du sport de l'utilisateur
+            const response = await axios.get(`https://maxrep-back.onrender.com/api/profile/${userId}` , {
                 headers: {
                     'Authorization': `Bearer ${token}` //Send token to backend to verify user
                 }
             });
             
-            // setRanking(response.data);
-            console.log('ranking :' , response.data);
+            console.log('userInfos :' , response.data);
+            setQueryParams({
+                ...queryParams,
+                sportId: response.data.sports[0].id,
+                gender: response.data.gender
+            })
+            getBestScores({
+                ...queryParams,
+                sportId: response.data.sports[0].id,
+                gender: response.data.gender
+            })
         }
 
         catch(error) {
@@ -128,9 +149,27 @@ const RankingPage = () => {
         }
     };
 
-    // const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    //     setUserSportID(parseInt(e.target.value));
-    // }
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate('/login');
+
+        } else {
+            // getUserInfos();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[isAuthenticated, navigate, token, userId]);
+
+    useEffect(() => {
+        getUserInfos();
+    }, [])
+
+    // useEffect(() => {
+    //     getBestScores(queryParams);
+    // }, [])
+
+    
+
+    
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         e.preventDefault();
@@ -141,17 +180,14 @@ const RankingPage = () => {
             ...prevParams,
             [name]: (name === "sportId" || name === "weightMin" || name === "weightMax") ? parseInt(value) : value
         }));
-        console.log(typeof queryParams.weightMin)
-
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(queryParams.weightMin , queryParams.weightMax)
-        //console.log(typeof queryParams.weightMin);
 
         if (queryParams.weightMin > queryParams.weightMax) {
-            console.log('Erreur !')
+            //! Afficher un message d'erreur sur le front
+            console.log('Erreur !') 
             return setError({status:0, message:'Le poids maximum doit etre supérieur au poids minimum !'}) 
         }
 
@@ -179,8 +215,9 @@ const RankingPage = () => {
                             <option value={4}> Deadlift </option>
                         </select>
                         <select name="country" id="" value={queryParams.country} onChange={handleChange}>
-                            <option value={'USA'}> USA </option>
-                            <option value={'France'}> France </option>
+                            {Object.entries(countryNames).map(([key, value]) => (
+                                <option key={key} value={value}>{value}</option>
+                            ))}
                         </select>
                         <label htmlFor="">Poids min</label>
                         <input name='weightMin'type='number' value={queryParams.weightMin} onChange={handleChange}></input>
@@ -188,6 +225,29 @@ const RankingPage = () => {
                         <input name='weightMax' type='number' value={queryParams.weightMax} onChange={handleChange}></input>
                         <button type='submit'> Valider </button>
                     </form>
+                    <table className='board'>
+                        <thead>
+                            <tr>
+                                <th>Rang</th>
+                                <th>Pays</th>
+                                <th>Nom Prénom</th>
+                                <th>Best Score</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {ranking.length > 0 ? (
+                            ranking.map((item: RankingProps, index) => (
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{item.user.country}</td>
+                                    <td>{item.user.firstname} {item.user.lastname}</td>
+                                    <td>{item.best_score}</td>
+                                    <td>{item.date}</td>
+                                </tr>
+                            ))) : null}
+                        </tbody>
+                    </table>
                 </main>
             </div>
             

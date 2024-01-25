b1c8788626837f24d3f92ee14c2caf591f3fc42b
unknown <antoine_grubert@hotmail.fr>
2024-01-25 10:51:09 +0100

    refacto : create a chart component and call it in perf page

--- src/pages/PerformancePage/PerformancePage.tsx
+++ src/pages/PerformancePage/PerformancePage.tsx
@@ -4,34 +4,11 @@ import NavMenu from '../../components/NavMenu/NavMenu';
 import NoPerfMessage from '../../components/NoPerfMessage/NoPerfMessage';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import Loader from '../../components/Loader/Loader';
-import 'chartjs-adapter-date-fns';
-import { Line } from 'react-chartjs-2';
+import Chart from '../../components/Chart/Chart';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import { convertSecondsToHMS } from '../../utils/convertTime';
 import axios from 'axios';
-import { Chart as ChartJS, 
-    CategoryScale, 
-    LinearScale, 
-    PointElement, 
-    LineElement, 
-    TimeScale, 
-    Title, 
-    Tooltip, 
-    Legend 
-} from 'chart.js';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    TimeScale,
-    Title,
-    Tooltip,
-    Legend
-);
 
 interface ErrorProps {
     status:number,
@@ -53,10 +30,6 @@ interface SessionProps {
     sport_id:number,
 }
 
-interface OpenStatus {
-    [key: number]: boolean;
-}
-
 const PerformancePage = () => {
 
     const navigate = useNavigate(); //Hook to navigate to another page
@@ -65,7 +38,6 @@ const PerformancePage = () => {
     const [userPerformances, setUserPerformances] = useState<SportProps[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<ErrorProps | null>(null);
-    const [isGraphOpen, setIsGraphOpen] = useState<OpenStatus>({});
 
     useEffect(() => {
         if (!isAuthenticated()) {
@@ -98,11 +70,6 @@ const PerformancePage = () => {
             //== Case response is ok
             if (response.status === 200) {
                 setUserPerformances(response.data.sports);
-                const initialOpenStatus: OpenStatus = {}
-                response.data.sports.forEach((sport: SportProps) => {
-                    initialOpenStatus[sport.id] = false;
-                })
-                setIsGraphOpen(initialOpenStatus);
 
             } else {
                 setError({status:500, message:'Internal Server Error / Erreur interne du serveur'})
@@ -129,100 +96,6 @@ const PerformancePage = () => {
         }
     }
 
-    const toggleOpen = (sportId: number) => {
-        setIsGraphOpen((prevStatus: OpenStatus) => ({
-            ...prevStatus,
-            [sportId]: !prevStatus[sportId]
-        }));
-    };
-
-    //! Sortir la partie Chart dans un module
-    const prepareChartData = (sport: SportProps) => {
-        const sortedSessions = sport.sessions.sort((a: SessionProps, b: SessionProps) => new Date(a.date).getTime() - new Date(b.date).getTime());
-        const dataPoints = sortedSessions.map(session => ({
-            x: new Date(session.date), // Convertir en objet Date
-            y: session.score
-        }));
-
-        console.log(dataPoints)
-    
-        return {
-            label: '',
-            data: dataPoints,
-            fill: false,
-            borderColor: '#E73725',
-            backgroundColor: '#E1E1E1',
-        };
-    };
-
-    const chartOptions = {
-        responsive: true,
-        scales: {
-            x: {
-                type: 'time' as const,
-                time: {
-                    unit: 'day' as const,
-                    displayFormats: {
-                        day: 'd MMM' as const
-                    }
-                },
-                title: {
-                    display: true,
-                    text: 'Date' as const
-                }
-            },
-            y: {
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: 'Score (kg)'
-                },
-            },
-        } , 
-        plugins: {
-            legend: {
-                display: false
-            },
-        },
-        
-    };
-
-    const chartOptionsTime = {
-        responsive: true,
-        scales: {
-            x: {
-                type: 'time' as const,
-                time: {
-                    unit: 'day' as const,
-                    displayFormats: {
-                        day: 'd MMM' as const
-                    }
-                },
-                title: {
-                    display: true,
-                    text: 'Date' as const
-                }
-            },
-            y: {
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: 'Score (temps)'
-                },
-                ticks: {
-                    callback: function(value: number) {
-                        return convertSecondsToHMS(value)
-                    }
-                }
-            },
-        }, 
-        plugins: {
-            legend: {
-                display: false
-            },
-        },
-    };
-
     //Handle 3 cases => error, loading and userInfos received
     if (error) {
         return <ErrorPage status={error.status} message={error.message} />
@@ -245,18 +118,7 @@ const PerformancePage = () => {
                                 <NoPerfMessage /> : (
                                 <div className="sports-list">
                                     {userPerformances && userPerformances.map((sport: SportProps) => (
-                                        <article key={sport.id} className="sport">
-                                            <div className="sport__header">
-                                                <h3> {sport.name} </h3>
-                                                <i className={`fa-solid fa-chevron-${isGraphOpen[sport.id] ? 'up' : 'down'}`} onClick={() => toggleOpen(sport.id)}></i>
-                                            </div>
-                                            <div className={`sport__content ${isGraphOpen[sport.id] ? '' : 'hide' }`}>
-                                                <Line 
-                                                    data={{ datasets: [prepareChartData(sport)] }} 
-                                                    options={sport.unit === 'temps' ? chartOptionsTime : chartOptions}
-                                                />
-                                            </div>
-                                        </article>
+                                        <Chart sport={sport} />
                                     ))}
                             </div>
                             )}

7b0b93688063d6668080877f155a3a77baedc4a2
unknown <antoine_grubert@hotmail.fr>
2024-01-26 11:48:31 +0100

    feat : improving home page + add logo + fixing paddings in register & login

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import './HomePage.scss';
 import Header from '../../components/Header/Header';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
@@ -16,25 +16,42 @@ const HomePage = () => {
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[isAuthenticated, navigate, token, userId])
+
+    //Handle scroll to second page
+    const secondPage = useRef<HTMLDivElement>(null);
+    const scrollToSecondPage = () => { 
+        if (secondPage.current) { 
+            secondPage.current.scrollIntoView({ behavior: 'smooth' }); 
+        }
+    }
     
     return (
         <div className='home-page'>
             <Header />
             <main className='home-main'>
-                <section className='home-firstpage'>
+                <section className='home-landing'>
                     <button type='button' className='button black'> 
                         <Link to={'/login'} className='white'> CONNEXION </Link> 
                     </button>
                     <button type='button' className='button white'> 
                         <Link to={'/register'} className='black'> INSCRIPTION </Link> 
                     </button>
-                    <div className="show-more">
+                    <div className="show-more" onClick={scrollToSecondPage}>
                         <p> Voir plus </p>
                         <i className="icon fa-solid fa-chevron-down"></i>
                     </div>
                 </section>
-                <section className='home-secondpage'>
-                    <p> Texte </p>
+                <section ref={secondPage} className='home-presentation'>
+                    <div className="content">
+                        <p> Présentation de l'appli </p>
+                        <div className="screenshots">
+                            <img src="/assets/images/screen-session.png" alt="session-page" />
+                            <img src="/assets/images/screen-performance.png" alt="performance-page" />
+                        </div>
+                    </div>
+                    <footer className='footer'>
+                        <p> Site créé par Charles, Thomas & Antoine... Contenu à définir </p>
+                    </footer>
                 </section>
             </main>
         </div>

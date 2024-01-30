8e91bd61f0a0c07104b66dea9fe418c361b1f816
unknown <antoine_grubert@hotmail.fr>
2024-01-30 10:12:30 +0100

    feat : add content in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import { useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
+import { useMediaQuery } from 'react-responsive';
 
 const HomePage = () => {
 
@@ -16,6 +17,10 @@ const HomePage = () => {
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[isAuthenticated, navigate, token, userId])
+
+    const isMobile = useMediaQuery({
+        query: '(max-width: 768px)'
+      }) 
 
     //Handle scroll to second page
     const secondPage = useRef<HTMLDivElement>(null);
@@ -41,16 +46,19 @@ const HomePage = () => {
                         <i className="icon fa-solid fa-chevron-down"></i>
                     </div>
                 </section>
-                <section ref={secondPage} className='home-presentation'>
-                    <div className="content">
-                        <p> Présentation de l'appli </p>
-                        <div className="screenshots">
+                <section  className='home-presentation'>
+                    <div  className="content">
+                        <h1 ref={secondPage} className='content__title'> MAXREP </h1>
+                        <p className='content__text'> Maxrep est l'outil idéal pour les passionnés de sport souhaitant suivre et améliorer leurs performances. <br/> <br />
+                        Cette application intuitive vous permet de suivre vos progrès dans diverses disciplines sportives, vous aidant à atteindre vos objectifs avec précision et motivation. </p>
+                        <div className="content__screenshots">
                             <img src="/assets/images/screen-session.png" alt="session-page" />
                             <img src="/assets/images/screen-performance.png" alt="performance-page" />
+                            {!isMobile && <img src="/assets/images/screen-ranking.png" alt="ranking-page" />}
                         </div>
                     </div>
                     <footer className='footer'>
-                        <p> Site créé par Charles, Thomas & Antoine... Contenu à définir </p>
+                        <p> Application développée par Charles Robart, Thomas Rugolino & Antoine Grubert. </p>
                     </footer>
                 </section>
             </main>
